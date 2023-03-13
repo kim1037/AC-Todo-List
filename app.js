8b1fdefa87ae6f64be22ebad5a90829ba313f7c9
kim1037 <pan913063@gmail.com>
2023-03-13 16:26:26 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== "production") {
+  //「非正式環境」 (亦指非 Production 正式機)
+  require("dotenv").config();
+}
 const app = express();
 const port = 3000;
+
+//connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+//連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("connect success!");
+});
 
 app.get("/", (req, res) => {
   res.send(`Express server test.`);
