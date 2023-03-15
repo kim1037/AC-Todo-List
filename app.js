c90f8826cf4e8e344db4997ceebcebd9adc723b7
kim1037 <pan913063@gmail.com>
2023-03-15 15:15:20 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
@@ -25,8 +26,12 @@ db.once("open", () => {
   console.log("connect success!");
 });
 
+//set view template
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" })); //設定副檔名
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-  res.send(`Express server test.`);
+  res.render(`index`);
 });
 
 app.listen(port, () => {
