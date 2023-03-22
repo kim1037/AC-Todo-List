229181c72ad9fbe64d5e7dc0677f014bf1d946d1
kim1037 <pan913063@gmail.com>
2023-03-22 14:18:57 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ name: "asc" }) //排序, 倒序 desc
     .then((todos) => {
       res.render(`index`, { todos }); // 將資料傳給 index 樣板
     })
