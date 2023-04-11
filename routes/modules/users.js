108d47312530b99f0f527c5dc4e8f3b00b2214e8
kim1037 <pan913063@gmail.com>
2023-04-11 18:15:30 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,5 +4,22 @@ const router = express.Router();
 router.get("/login", (req, res) => {
   res.render("login");
 });
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  Users.findOne({ email, password })
+    .then((user) => {
+      if (user) {
+        res.render("index", { firstName: user.firstName });
+      } else {
+        const errorMessage = `Wrong email or password, please try again.`;
+        res.render("login", { errorMessage });
+      }
+    })
+    .catch((e) => console.log(e));
+});
 
 module.exports = router;
