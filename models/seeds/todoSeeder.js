8f8511bfe7daba9c1b968308c657aa8aec72a464
kim1037 <pan913063@gmail.com>
2023-04-18 17:17:19 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,8 +1,37 @@
+const bcrypt = require("bcryptjs");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config;
+}
 const Todo = require("../todo"); // 載入 todo model
-const db = require('../../config/mongoose')
+const User = require("../user");
+const db = require("../../config/mongoose");
+const SEED_USER = {
+  name: "tester",
+  email: "tester@test.com",
+  password: "test123",
+};
+
 db.once("open", () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` });
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) =>
+          Todo.create({ name: `name-${i}`, userId })
+        )
+      );
+    })
+    .then(() => {
+      console.log("done");
+      process.exit(); //產生完之後關閉這段 Node 執行程序
+    });
 });
